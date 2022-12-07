0546948adcfb9fb1fe4b38dd0f6cddcba6d898d4
ashkhendavtyan2022 <119871908+ashkhendavtyan2022@users.noreply.github.com>
2022-12-07 21:02:09 +0400

    lesson 07-12-2022

--- react-app/src/Components/box/index.jsx
+++ react-app/src/Components/box/index.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import "./style.css";
 
-const Boxes = () => {
-    return <div>
+// This is a Function Component
+// const Boxes = () => {
+//     return <div>
+//         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente facere in eius eligendi debitis, laborum?</h1>
+//     </div>
+// }
+
+// export default Boxes
+
+//This is Class Component
+
+class Boxes extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            number: 45,
+            f: 12
+        }
+        /* 
+        state = {
+            f: 7,
+            number: 45
+        }
+        */
+    }
+
+    change = () => {
+        this.setState({f : this.state.number + this.state.f}); 
+    }
+
+    render() {
+        return <div>
+            {console.log(this.state.f)}
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente facere in eius eligendi debitis, laborum?</h1>
-    </div>
+        <p>{this.state.f}</p>
+        <button onClick={this.change}>Change</button>
+     </div>
+    }
 }
 
-export default Boxes;
+export default Boxes
