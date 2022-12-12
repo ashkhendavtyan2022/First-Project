1b26daf59fa65fc392e38ddb49c2b244d6a1d777
ashkhendavtyan2022 <119871908+ashkhendavtyan2022@users.noreply.github.com>
2022-12-12 15:28:55 +0400

    12-12-2022 ed

--- react-app/src/Components/Input/index.jsx
+++ react-app/src/Components/Input/index.jsx
@@ -3,43 +3,72 @@ import "./style.css"
 
 class Input extends React.Component {
     constructor(props) {
-        super(props)
-        this.state = {
-            changeBy: 1,
-            number: 0
-        }
+      super(props);
+      this.state = {
+        changeBy: 1,
+        number: 0,
+      };
     }
-
+  
     enterNumber = (e) => {
-        this.setState({number: e.target.value})  
-    }
-    
+      this.setState((prev) => {
+        return {
+          ...prev,
+          number: e.target.value,
+        };
+      });
+    };
+  
     increaseNumber = () => {
-        this.setState({number: this.state.number + this.state.changeBy})
-    }
-
+      this.setState((prev) => {
+        return {
+          ...prev,
+          number: +prev.number + +prev.changeBy,
+        };
+      });
+    };
+  
     decreaseNumber = () => {
-        if(this.state.number === 0) {
-            this.setState({number: this.state.number})
-        } else {
-            this.setState({number : this.state.number - this.state.changeBy});
-        }
-         
-    }
-
-    // newNumber = (e) => {
-    //     this.setState({changeBy: e.target.value})
-    // }
-
-    render () {
-        return <div className="input-margin">
-            <button className="btn" onClick={this.increaseNumber}>+</button>
-            <input type="number" onChange={this.enterNumber}/>
-            <button className="btn" onClick={this.decreaseNumber}>-</button>
-
-            <input type="number" className="input-new-number" onChange={this.newNumber} />
+      this.setState((prev) => {
+        return {
+          ...prev,
+          number: +prev.number - +prev.changeBy,
+        };
+      });
+    };
+  
+    newNumber = (e) => {
+      this.setState((prev) => {
+        return {
+          ...prev,
+          changeBy: e.target.value,
+        };
+      });
+    };
+  
+    render() {
+      return (
+        <div className="input-margin">
+          <button className="btn" onClick={this.increaseNumber}>
+            +
+          </button>
+          <input
+            type="number"
+            onChange={this.enterNumber}
+            value={this.state.number}
+          />
+          <button className="btn" onClick={this.decreaseNumber}>
+            -
+          </button>
+  
+          <input
+            type="number"
+            className="input-new-number"
+            onChange={this.newNumber}
+          />
         </div>
+      );
     }
-}
-
-export default Input
+  }
+  
+  export default Input;
