a8f0ea887127c3ade00ff7c8cf81fd2aa0cd6a9e
ashkhendavtyan2022 <119871908+ashkhendavtyan2022@users.noreply.github.com>
2022-12-12 20:26:24 +0400

    das 12-12-2022

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Boxes from "./Components/box";
 import Header from "./Components/Header";
 import Input from "./Components/Input";
+import Handle from "./Components/Input";
+import Paysage from "./assets/images/img1.jpg"
 
 function App() {
   return (
@@ -9,7 +11,8 @@ function App() {
       <Header />
       <Boxes />
       <Input />
-      
+      <Handle text="This is something we write in a specific component" />
+      <Handle image={Paysage}/>
     </div>
   );
 }
--- react-app/src/Components/Input/index.jsx
+++ react-app/src/Components/Input/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css"
 
+/*
 class Input extends React.Component {
     constructor(props) {
       super(props);
@@ -78,3 +79,54 @@ class Input extends React.Component {
   }
   
   export default Input;
+
+*/
+class Handle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        number: 0,
+        value: 0,
+      }
+    }
+
+    Plus = () => {
+        this.setState({number: this.state.number + this.state.value})
+    }
+    
+    Minus = () => {
+        if(this.state.number <= 0) {
+            this.setState({number: 0})
+        } else {
+            this.setState({number: this.state.number - this.state.value })
+        }
+    }
+
+    handleChange = (e) => {
+        if(e.target.value <= 0) {
+            this.setState({value: 1})
+        } else {
+            this.setState({value: +e.target.value})
+        }
+        
+    }
+
+      render () {
+        const{text, image} = this.props
+
+        return (
+            <div className="demo">
+                <button onClick={this.Plus}>+</button>
+                <div className={image? "block" : "block2"}>{this.state.number}</div>
+                <button onClick={this.Minus}>-</button>
+                <input type="number" onChange={this.handleChange} />
+                <p>{text}</p>
+                {image ? <img src={image} alt="nkar" /> : null}
+           </div>
+        )
+      }
+}
+
+  export default Handle;
+
+  
